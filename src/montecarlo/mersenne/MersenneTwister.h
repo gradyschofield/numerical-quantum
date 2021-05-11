cca2e47a98aea1d04004eb35eba03ac3d586aea8
Grady Schofield <grady.schofield@gmail.com>
2021-05-11 17:42:24 -0500

    rearrange MT code

---
+++ src/montecarlo/mersenne/MersenneTwister.h
@@ -0,0 +1,89 @@
+//
+// Created by Grady Schofield on 5/11/21.
+//
+
+#ifndef ATOM_MERSENNETWISTER_H
+#define ATOM_MERSENNETWISTER_H
+
+#include<vector>
+
+using namespace std;
+
+class MersenneTwister {
+    /*
+     * The naming convention follows the paper http://www.math.sci.hiroshima-u.ac.jp/m-mat/MT/ARTICLES/mt.pdf
+     * See the algorithm pseudocode on page 7 and Table 2 page 8 of that paper.
+     */
+    vector<uint32_t> state;
+    int n;
+    int m;
+    int a;
+    int u;
+    int s;
+    int t;
+    int l;
+    int b;
+    int c;
+
+    int lastPos = 0;
+    uint32_t upperMask;
+    uint32_t lowerMask;
+public:
+
+    static MersenneTwister createMT19937() {
+        MersenneTwister mt19937(624, 397, 31, 0x9908b0df, 11, 7, 15, 18, 0x9d2c5680, 0xefc60000);
+        mt19937.initState();
+        return mt19937;
+    }
+
+    MersenneTwister(int n, int m, int r, int a, int u, int s, int t, int l, int b, int c)
+            : state(n), n(n), m(m), a(a), u(u), s(s), t(t), l(l), b(b), c(c)
+    {
+        upperMask = ((uint32_t)~0) << (32-r);
+        lowerMask = ((uint32_t)~0) >> r;
+    }
+
+    void initState() {
+        for(uint32_t & x : state) {
+            x = rand();
+        }
+    }
+
+    uint32_t generateUntempered() {
+        uint32_t first = state[lastPos];
+        uint32_t second = state[lastPos + 1 >= n ? lastPos + 1 - n : lastPos + 1];
+        uint32_t mth = state[lastPos + m >= n ? lastPos + m - n : lastPos + m];
+
+        uint32_t t1 = (upperMask & first) | (lowerMask & second);
+        uint32_t t2 = mth ^ (t1 >> 1);
+        uint32_t x = (t1 & 0x1) == 0 ? t2 : t2 ^ a;
+        state[lastPos] = x;
+        lastPos = lastPos + 1 == n ? 0 : lastPos + 1;
+        return x;
+    }
+
+    uint32_t generate() {
+        uint32_t x = generateUntempered();
+        uint32_t y = x ^ (x >> u);
+        y = y ^ ((y << s) & b);
+        y = y ^ ((y << t) & c);
+        y = y ^ (y >> l);
+        return y;
+    }
+
+    float generateFloat() {
+        static float maxInv = 1.0f / (float)numeric_limits<uint32_t>::max();
+        uint32_t t = generate();
+        return t * maxInv;
+    }
+
+    double generateDouble() {
+        static double maxInv = 1.0 / (double)numeric_limits<uint64_t>::max();
+        uint64_t t1 = generate();
+        uint64_t t2 = generate();
+        return (t1 | t2 << 32) * maxInv;
+    }
+};
+
+
+#endif //ATOM_MERSENNETWISTER_H
--- src/montecarlo/mersenne/TestJumpAhead.cpp
+++ src/montecarlo/mersenne/TestJumpAhead.cpp
@@ -2,6 +2,13 @@
 // Created by Grady Schofield on 5/10/21.
 //
 
+#include<iostream>
+#include<unordered_set>
+#include<vector>
+#include<tuple>
+
+using namespace std;
+
 /*
  * This program implements the fast jump ahead idea in
  * http://www.iro.umontreal.ca/~lecuyer/myftp/papers/jumpf2.pdf .
@@ -9,14 +16,186 @@
  * to compare the fast code for correctness.
  */
 
+class MatrixElement {
+    int row;
+    int column;
+
+public:
+
+    MatrixElement() = delete;
+
+    MatrixElement(MatrixElement && e)
+        : row(e.getRow()), column(e.getColumn())
+    {
+    }
+
+    MatrixElement(MatrixElement const & e)
+        : row(e.getRow()), column(e.getColumn())
+    {
+    }
+
+    MatrixElement(int row, int column)
+        : row(row), column(column)
+    {
+    }
+
+    int getRow() const {
+        return row;
+    }
+
+    int getColumn() const {
+        return column;
+    }
+
+    bool operator==(MatrixElement const & e) const {
+        return row == e.getRow() && column == e.getColumn();
+    }
+};
+
+namespace std {
+    template<> struct hash<MatrixElement>
+    {
+        size_t operator()(MatrixElement const & elem) const noexcept {
+            return elem.getRow() ^ (elem.getColumn() << 1);
+        }
+    };
+}
+
+/*
+ * The matrix is stored in column major ordering.  The first word is the first 32 rows of column 1.
+ */
+class DenseF2Matrix {
+    vector<uint32_t> elements;
+    int dim;
+    int wordsPerColumn;
+public:
+
+    DenseF2Matrix(int dim)
+        : dim(dim), wordsPerColumn(dim/32), elements(wordsPerColumn*dim)
+    {
+    }
+
+    DenseF2Matrix(vector<uint32_t> && elements) {
+        if((int)sqrt(elements.size()) != sqrt(elements.size())) {
+            throw runtime_error("Tried to construct a nonsquare DenseF2Matrix");
+        }
+        dim = sqrt(elements.size());
+        DenseF2Matrix::elements = move(elements);
+    }
+
+    DenseF2Matrix square() const {
+    }
+
+    void setElement(int row, int column) {
+        int columnWord = column / 32;
+        int columnBit = column % 32;
+        elements[row * wordsPerColumn + columnWord] |= 1 << columnBit;
+    }
+
+    uint32_t getElement(int row, int column) const {
+        int columnWord = column / 32;
+        int columnBit = column % 32;
+        return (elements[row * wordsPerColumn + columnWord] & (1 << columnBit)) == 0 ? 0 : 1;
+    }
+};
+
 /*
  * This class will help us build up the recurrence matrix for a Mersenne twister generator
  * using a code that is fairly easy to read.
  */
 class SparseMatrix {
+    unordered_set<MatrixElement> elements;
 
+public:
+    void insert(int row, int col) {
+        elements.emplace(row, col);
+    }
+
+    int getNumElements() const {
+        return elements.size();
+    }
+
+    DenseF2Matrix bake(int w) const {
+        int maxRow = 0;
+        int maxColumn = 0;
+        for(auto & p : elements) {
+            maxRow = max(maxRow, p.getRow());
+            maxColumn = max(maxColumn, p.getColumn());
+        }
+
+        if(maxRow != maxColumn) {
+            throw runtime_error("Max row didn't match max column in SparseMatrix::bake");
+        }
+
+        if(maxRow % w != 0) {
+            throw runtime_error("Max row is not a multiple of w");
+        }
+
+        if(maxColumn % w != 0) {
+            throw runtime_error("Max column is not a multiple of w");
+        }
+
+        DenseF2Matrix matrix(maxRow);
+        for(auto & p : elements) {
+            matrix.setElement(p.getRow(), p.getColumn());
+        }
+        return matrix;
+    }
 };
 
 int main(int argc, char ** argv) {
+    int n = 624;
+    int w = 32;
+    int r = 31;
+    uint32_t a = 0x9908b0df;
+    int rowOffset = 0;
+    int columnOffset = 0;
+    SparseMatrix A;
+    for(int z = 0; z < n; ++z) {
+        /*
+         * x_k = x_k+m + x_1^u | x_2^l)
+         * 0 0 0 ... 0 0 a31
+         * 1 0 0 ... 0 0 a30
+         * 0 1 0 ... 0 0 a29
+         * 0 0 1 ... 0 0 a28
+         * . . . .   . .  .
+         * . . .  .  . .  .
+         * . . .   . . .  .
+         * 0 0 0 ... 1 0  a1
+         * 0 0 0 ... 0 1  a0
+         */
+
+        /*
+         * Handle the upper w-r bits of x_k
+         */
+        for(int row = rowOffset + 1; row < rowOffset + w - r; ++row) {
+            int col = row - 1;
+            A.insert(row, col);
+        }
+
+        /*
+         * Handle the lower r bits of x_(k+1)
+         */
+        for(int row = rowOffset + w - r; row < rowOffset + w; ++row) {
+            int col = row + w - 1;
+            A.insert(row, col);
+        }
+
+        for(int i = 0; i < w; ++i) {
+            int row = rowOffset + i;
+            int col = columnOffset + 2*w;
+            // The low order bit of 'a' is in the lower left corner of the matrix, hence a shift of w-i
+            if(a & (1 << (w-i))) {
+                A.insert(row, col);
+            }
+        }
+
+        columnOffset += w;
+        rowOffset += w;
+    }
+
+    DenseF2Matrix denseMatrix = A.bake(w);
+
+    cout << "Num nonzero elements in A " << A.getNumElements() << "\n";
     return 0;
 }
