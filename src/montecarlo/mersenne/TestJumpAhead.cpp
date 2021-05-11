7ebd7b4c468d285d9f9039aaa5ac0262f4ad169d
Grady Schofield <grady.schofield@gmail.com>
2021-05-11 14:31:13 -0500

    Add jump ahead program

---
+++ src/montecarlo/mersenne/TestJumpAhead.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Grady Schofield on 5/10/21.
+//
+
+/*
+ * This program implements the fast jump ahead idea in
+ * http://www.iro.umontreal.ca/~lecuyer/myftp/papers/jumpf2.pdf .
+ * It also implements the slower straightforward way to jump ahead as a reference
+ * to compare the fast code for correctness.
+ */
+
+/*
+ * This class will help us build up the recurrence matrix for a Mersenne twister generator
+ * using a code that is fairly easy to read.
+ */
+class SparseMatrix {
+
+};
+
+int main(int argc, char ** argv) {
+    return 0;
+}
